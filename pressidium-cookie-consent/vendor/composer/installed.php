b45945dea6c23b79ae60096df2cda6a2a163ec38
Sean Murphy <sean@iamseanmurphy.com>
2024-09-25 00:56:46 +0000

    1.5.2

---
+++ pressidium-cookie-consent/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pressidium/pressidium-cookie-consent',
+        'pretty_version' => '1.5.2',
+        'version' => '1.5.2.0',
+        'reference' => '0b4749ad1f3f6d69069085b9436ab48b781bcc4a',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'pressidium/pressidium-cookie-consent' => array(
+            'pretty_version' => '1.5.2',
+            'version' => '1.5.2.0',
+            'reference' => '0b4749ad1f3f6d69069085b9436ab48b781bcc4a',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
